4073ed05af19313684997615be60bc81b8dc4c32
GalaxyFruit <jindrawallach@gmail.com>
2024-12-15 18:15:54 +0100

    test2

--- Astraltica Project/Assets/Scripts/InventoryItem.cs
+++ Astraltica Project/Assets/Scripts/InventoryItem.cs
@@ -7,52 +7,24 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     [Header("UI")]
     public Image image;
 
-    private Transform parentAfterDrag;
-    private CanvasGroup canvasGroup;
-
-    private void Awake()
-    {
-        canvasGroup = GetComponent<CanvasGroup>();
-        if (canvasGroup == null)
-        {
-            canvasGroup = gameObject.AddComponent<CanvasGroup>();
-        }
-    }
+    [HideInInspector] Transform parentAfterDrag;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        // Upravíme UI pro dragování
         image.raycastTarget = false;
-        canvasGroup.alpha = 0.6f; // Průhlednost
-
-        parentAfterDrag = transform.parent; // Uložení původního rodiče
-        transform.SetParent(transform.root); // Přesun na nejvyšší úroveň UI
-        transform.SetAsLastSibling(); // Zajistí, že je položka nahoře
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        //transform.SetAsLastSibling();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        // Následuje pozici myši
         transform.position = Input.mousePosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         image.raycastTarget = true;
-        canvasGroup.alpha = 1.0f;
-
-        // Ověříme, zda je položka nad platným slotem
-        if (eventData.pointerEnter != null && eventData.pointerEnter.CompareTag("Slot"))
-        {
-            // Přesun položky do nového slotu
-            transform.SetParent(eventData.pointerEnter.transform);
-        }
-        else
-        {
-            // Vrácení položky na původní pozici
-            transform.SetParent(parentAfterDrag);
-        }
-
-        transform.localPosition = Vector3.zero; // Zarovnání na střed slotu
+        transform.SetParent(parentAfterDrag);
     }
 }
