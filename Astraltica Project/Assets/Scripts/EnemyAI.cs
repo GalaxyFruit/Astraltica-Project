aad546af721dbb0a8f673cacee2b025d5e93ab2a
GalaxyFruit <jindrawallach@gmail.com>
2025-01-20 19:15:02 +0100

    Added enemy AI walk

--- Astraltica Project/Assets/Scripts/EnemyAI.cs
+++ Astraltica Project/Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
@@ -8,42 +9,66 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] private float checkTime = 1f;
 
     private EnemyAnimationController animationController;
+    private NavMeshAgent agent;
     private float timer = 0;
     private bool isDead = false;
 
     private void Start()
     {
         animationController = GetComponent<EnemyAnimationController>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = attackRange;
     }
 
     private void Update()
     {
         timer -= Time.deltaTime;
-        if(timer < 0)
+        if (timer > 0) return;
+
+        timer = checkTime;
+
+        if (isDead) return;
+
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (distance <= attackRange)
         {
-            if (isDead) return;
-
-            float distance = Vector3.Distance(transform.position, player.position);
-
-            if (distance <= attackRange)
-            {
-                float attackType = Random.Range(3f, 5f); // Left Punch = 3, Right Punch = 4
-                animationController.SetAction(attackType);
-            }
-            else if (distance <= detectionRange)
-            {
-                animationController.SetAction(2); // Run
-            }
-            else
-            {
-                animationController.SetAction(0); // Idle
-            }
+            AttackPlayer();
         }
+        else if (distance <= detectionRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            Idle();
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+        animationController.SetAction(2); // Run
+    }
+
+    private void Idle()
+    {
+        agent.ResetPath();
+        animationController.SetAction(0); // Idle
+    }
+
+    private void AttackPlayer()
+    {
+        agent.ResetPath();
+
+        float attackType = Random.Range(3f, 5f); // Left Punch = 3, Right Punch = 4
+        animationController.SetAction(attackType);
     }
 
     public void Kill()
     {
         isDead = true;
+        agent.ResetPath();
         animationController.Die();
     }
 }
