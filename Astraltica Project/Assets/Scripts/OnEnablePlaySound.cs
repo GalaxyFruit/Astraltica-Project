587004b83fcef6cf1e446079080524ac9b59e90c
JindraWallach <149482038+GalaxyFruit@users.noreply.github.com>
2025-04-29 08:20:55 +0200

    Added cutscenen sounds

---
+++ Astraltica Project/Assets/Scripts/OnEnablePlaySound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class OnEnablePlaySound : MonoBehaviour
+{
+    [SerializeField]
+    private string soundClipName;
+
+    private void OnEnable()
+    {
+        CallMyMethod();
+    }
+
+    private void CallMyMethod()
+    {
+        if (!string.IsNullOrEmpty(soundClipName))
+        {
+            AudioManager.Instance?.PlaySound(soundClipName, transform.position);
+        }
+        else
+        {
+            Debug.LogWarning("Název zvukového klipu není nastaven!");
+        }
+    }
+}
