007c7e5e5567aa53712857860ff1b3b2e3ea8840
GalaxyFruit <jindrawallach@gmail.com>
2025-06-01 21:23:51 +0200

    StarGate Material

---
+++ Astraltica Project/Assets/Scripts/MoveMaterialTexture.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+    public Renderer targetRenderer;
+    public Vector2 scrollSpeed = new Vector2(0.5f, 0f);
+    private Vector2 currentOffset;
+
+    void Update()
+    {
+        currentOffset += scrollSpeed * Time.deltaTime;
+
+        currentOffset.x %= 0.05f;
+        currentOffset.y %= 0.08f;
+
+        targetRenderer.material.SetTextureOffset("_BaseMap", currentOffset);
+
+
+    }
+}
