963f118968b432dd0944f444efa75d6a1fa10465
GalaxyFruit <jindrawallach@gmail.com>
2025-06-02 19:15:08 +0200

    Added AlphaKeycard

--- Astraltica Project/Assets/Ukolnicek.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-public class Ukolnicek : MonoBehaviour
-{
-    [Header("Seznam úkolů")]
-    public List<string> ukoly = new List<string>();
-
-}
