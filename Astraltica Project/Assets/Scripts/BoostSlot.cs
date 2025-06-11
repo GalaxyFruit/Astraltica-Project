b35f1660fbf18a5e975bd65b036133038ab39adf
JindraWallach <149482038+GalaxyFruit@users.noreply.github.com>
2025-06-11 16:06:21 +0200

    FX starting with pipes

--- Astraltica Project/Assets/Scripts/BoostSlot.cs
+++ Astraltica Project/Assets/Scripts/BoostSlot.cs
@@ -5,19 +5,19 @@ public class BoostSlot : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
-        Debug.Log("BoostSlot.OnDrop");
+        //Debug.Log("BoostSlot.OnDrop");
         if (transform.childCount > 1)
             return;
-        Debug.Log("1");
+        //Debug.Log("1");
         var item = eventData.pointerDrag.GetComponent<InventoryItem>();
         if (item == null || item.itemType != ItemType.Potion)
             return;
-        Debug.Log("2");
+        //Debug.Log("2");
         var activeType = BoostManager.Instance.GetActiveCrystalType();
-        Debug.Log("3");
+        //Debug.Log("3");
         if (activeType != CrystalType.None && activeType != item.crystalType)
             return;
-        Debug.Log("4");
+        //Debug.Log("4");
         BoostManager.Instance.AddPotion(item.crystalType);
         Destroy(item.gameObject);
         BoostManager.Instance.StartDepleting();
